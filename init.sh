06f17a0196024dc29a112032cb4ac60c34f320b1
alex <j_alex79@163.com>
2024-04-17 21:23:35 +0000

    add initialization bash file

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd 3rdparty; cd cutlass-extension; mkdir build; cd build; cmake ..
+
+if [ "$(nproc)" -le 16 ]; then
+    threads=$num_processors
+else
+    threads=16
+fi
+make -j"$threads"
+
+cd ../../../; python setup_cuda.py install
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/3rdparty/cutlass-extension/build/lib"
+
+pip3 install -r requirements.txt
